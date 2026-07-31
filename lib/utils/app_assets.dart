79a92a9bc4ec4f2799f84482fd51d30d671dc1b9
ayush <ayush@dicor.in>
2026-07-31 16:10:27 +0530

    minor changes

--- lib/utils/app_assets.dart
+++ lib/utils/app_assets.dart
@@ -1,7 +1,7 @@
 class AppAssets {
   // static const String appLogo = 'assets/images/app_logo.png';
   static const String globenexLogo          = 'assets/images/Globenex_Logo.jpeg';
-  static const String appLogo               = 'assets/images/digitalerp_newlogo.png';
+  static const String appLogo               = 'assets/images/Globenex_Logo.jpeg';
   static const String splashBg              = 'assets/images/splash_bg.png';
   static const String splashScreen          = 'assets/images/splash_screen.png';
   static const String setupBg               = 'assets/images/setup_bg.png';
